59a9f51581e7ae66aed67385ccc400247eb5f9d5
Rahulkrsingh32 <rahulkrsingh32@gmail.com>
2022-07-24 20:26:33 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['encrypted-tbn0.gstatic.com','lh3.googleusercontent.com'],
+  }
 }
 
 module.exports = nextConfig
