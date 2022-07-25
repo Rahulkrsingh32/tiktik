f9df4d5fb56cfe971501839ee49a788d52da29fe
Rahulkrsingh32 <rahulkrsingh32@gmail.com>
2022-07-25 09:53:54 +0530

    again ..

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  
   reactStrictMode: true,
   images: {
     domains: ['encrypted-tbn0.gstatic.com','lh3.googleusercontent.com'],
